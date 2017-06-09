389f324824c81e73323ba46ac19bcdaf3a5e9e09
nabeliwo <hiroki.c.watanabe@gmail.com>
2017-06-09 22:34:11 +0900

    オライリー1章終了

--- oreilly/src/js/chapter01.js
+++ oreilly/src/js/chapter01.js
@@ -1,44 +1,83 @@
 import * as THREE from 'three';
+import Stats from 'stats.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
+window.addEventListener('resize', onResize, false);
+
+function onResize() {
+  camera.aspect = innerWidth / innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(innerWidth, innerHeight);
+}
+
+function initStats() {
+  const stats = new Stats();
+  stats.setMode(0);
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+  document.getElementById('Stats-output').appendChild(stats.domElement);
+  return stats;
+}
 
 function init() {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 1000);
-  const renderer = new THREE.WebGLRenderer();
+  const stats = initStats();  
+  const controls = new function() {
+    this.rotationSpeed = 0.02;
+    this.bouncingSpeed = 0.03;
+  };
+
+  const gui = new dat.GUI();
+  gui.add(controls, 'rotationSpeed', 0, 0.5);
+  gui.add(controls, 'bouncingSpeed', 0, 0.5);
+
 
   renderer.setClearColor(new THREE.Color(0xEEEEEE));
   renderer.setSize(innerWidth, innerHeight);
+  renderer.shadowMap.enabled = true;
 
   const axes = new THREE.AxisHelper(20);
   scene.add(axes);
 
+  const spotLight = new THREE.SpotLight(0xffffff);
+  spotLight.position.set(-20, 30, -5);
+  spotLight.castShadow = true;
+  scene.add(spotLight);
+
   const planeGeometry = new THREE.PlaneGeometry(60, 20);
-  const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc });
+  const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xcccccc });
   const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
   plane.rotation.x = -0.5 * Math.PI;
   plane.position.x = 15;
   plane.position.y = 0;
   plane.position.z = 0;
+  plane.receiveShadow = true;
 
   scene.add(plane);
 
   const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-  const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+  const cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 });
   const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
   cube.position.x = -4;
   cube.position.y = 3;
   cube.position.z = 0;
+  cube.castShadow = true;
 
   scene.add(cube);
 
   const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-  const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x7777ff, wireframe: true });
+  const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
   const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
   sphere.position.x = 20;
   sphere.position.y = 4;
   sphere.position.z = 2;
+  sphere.castShadow = true;
 
   scene.add(sphere);
 
@@ -47,9 +86,24 @@ function init() {
   camera.position.z = 30;
   camera.lookAt(scene.position);
 
-  document.getElementById('WebGL-output').appendChild(renderer.domElement);
+  let step = 0;
 
-  renderer.render(scene, camera);
+  function renderScene() {
+    cube.rotation.x += controls.rotationSpeed;
+    cube.rotation.y += controls.rotationSpeed;
+    cube.rotation.z += controls.rotationSpeed;
+
+    step += controls.bouncingSpeed;
+    sphere.position.x = 20 + (10 * Math.cos(step));
+    sphere.position.y = 2 + (10 * Math.abs(Math.sin(step)));
+
+    stats.update();
+    requestAnimationFrame(renderScene);
+    renderer.render(scene, camera);
+  }
+
+  document.getElementById('WebGL-output').appendChild(renderer.domElement);
+  renderScene();
 }
 
 onload = init;
